d6d3720478f9b3bf016b28a8655b69f3329082e8
Kartik Shankhavaram <shankhavaramk@gmail.com>
2017-11-11 22:33:41 +0530

    Added spinner and refresh

--- DelhiMetroPathFinder/app/src/main/java/com/darksidehacks/delhimetropathfinder/activity/MainActivity.java
+++ DelhiMetroPathFinder/app/src/main/java/com/darksidehacks/delhimetropathfinder/activity/MainActivity.java
@@ -4,9 +4,17 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.darksidehacks.delhimetropathfinder.R;
@@ -27,19 +35,31 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> stations = new ArrayList<>();
+    ArrayList<StationInfo> stations = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    ArrayList<String> stationNames = new ArrayList<>();
 
     @BindView(R.id.main_toolbar) Toolbar toolbar;
     @BindView(R.id.source) AutoCompleteTextView source;
     @BindView(R.id.destination) AutoCompleteTextView destination;
-    @OnClick(R.id.get_route) void validate() {
-        String a = String.valueOf(source.getText());
-        String b = String.valueOf(destination.getText());
-        if(!stations.contains(a)) {
-            source.setError("Station does not exist.");
-        }
-        if(!stations.contains(b)) {
-            destination.setError("Station does not exist.");
+    @BindView(R.id.loading_icon) ProgressBar loadingSpinner;
+    @BindView(R.id.get_route) Button showRoute;
+    @BindView(R.id.message) TextView message;
+    @BindView(R.id.refreshButton) ImageButton refreshButton;
+    @OnClick(R.id.refreshButton) void refresh() {
+        loadingSpinner.setVisibility(View.VISIBLE);
+        refreshButton.setVisibility(View.GONE);
+        showRoute.setVisibility(View.GONE);
+        message.setText(R.string.loading_message);
+        message.setTextColor(getResources().getColor(R.color.colorLoadingMessage));
+        loadStationData();
+    }
+
+    @OnClick(R.id.get_route) void getRoute() {
+        if(!validate()) {
+            return;
+        } else {
+
         }
     }
 
@@ -52,17 +72,36 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
-        //startActivity(new Intent(this, SearchResultsActivity.class));
+        loadingSpinner.setVisibility(View.VISIBLE);
+        refreshButton.setVisibility(View.GONE);
+        showRoute.setVisibility(View.GONE);
+        message.setText(R.string.loading_message);
+        message.setTextColor(getResources().getColor(R.color.colorLoadingMessage));
 
-        stations.add("Rajiv Chowk");
-        stations.add("Rajiv Gali");
+        source.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+        destination.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stations);
-        source.setAdapter(adapter);
-        destination.setAdapter(adapter);
+        destination.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    switch (keyCode)
+                    {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            getRoute();
+                            Log.i("Done", "Pressed!");
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
 
-
-
+        loadStationData();
 
     }
 
@@ -99,7 +138,105 @@ public class MainActivity extends AppCompatActivity {
                 return result;
 
             } catch (Exception e) {
-                Toast.makeText(MainActivity.this, "Couldn't fetch station data, please try again.", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+                return null;
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if(s == null) {
+                message.setText(R.string.station_error_message);
+                refreshButton.setVisibility(View.VISIBLE);
+                message.setTextColor(getResources().getColor(R.color.colorErrorMessage));
+                loadingSpinner.setVisibility(View.GONE);
+                showRoute.setVisibility(View.VISIBLE);
+                return;
+            }
+
+            String crappyPrefix = "null";
+
+            if(s.startsWith(crappyPrefix)){
+                s = s.substring(crappyPrefix.length(), s.length());
+            }
+
+            int i;
+            StationInfo info;
+
+            message.setText("");
+
+            try {
+                JSONArray station = new JSONArray(s);
+                for(i = 0; i < station.length(); i++) {
+                    info = new StationInfo();
+                    info.setName(station.getJSONObject(i).getString("name"));
+                    info.setLine(station.getJSONObject(i).getString("line"));
+                    info.setIndex(station.getJSONObject(i).getInt("index"));
+                    JSONArray a = station.getJSONObject(i).getJSONArray("vertices");
+                    ArrayList<Integer> b = new ArrayList<>();
+                    for(int j = 0; j < a.length(); j++) {
+                        b.add(a.getInt(j));
+                    }
+                    info.setVertices(b);
+                    stations.add(info);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.i("JSON", "Couldn't do it!");
+            }
+
+            Log.i("Station", stations.toString());
+            for(i = 0; i < stations.size(); i++) {
+                stationNames.add(stations.get(i).getName());
+            }
+            adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, stationNames);
+            source.setAdapter(adapter);
+            destination.setAdapter(adapter);
+
+            loadingSpinner.setVisibility(View.GONE);
+            showRoute.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    public class GetShortestRoute extends AsyncTask<String, Void, String> {
+
+        HttpURLConnection httpURLConnection;
+        URL url1;
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            try {
+
+                url1 = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url1.openConnection();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                String result = "";
+
+                int data = reader.read();
+                char current;
+
+                while(data != -1) {
+
+                    current = (char) data;
+                    result += current;
+                    data = reader.read();
+
+                }
+
+
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
                 return null;
             }
 
@@ -116,9 +253,65 @@ public class MainActivity extends AppCompatActivity {
                 s = s.substring(crappyPrefix.length(), s.length());
             }
 
+            int i;
+            StationInfo info;
 
+            try {
+                JSONArray station = new JSONArray(s);
+                for(i = 0; i < station.length(); i++) {
+                    info = new StationInfo();
+                    info.setName(station.getJSONObject(i).getString("name"));
+                    info.setLine(station.getJSONObject(i).getString("line"));
+                    info.setIndex(station.getJSONObject(i).getInt("index"));
+                    JSONArray a = station.getJSONObject(i).getJSONArray("vertices");
+                    ArrayList<Integer> b = new ArrayList<>();
+                    for(int j = 0; j < a.length(); j++) {
+                        b.add(a.getInt(j));
+                    }
+                    info.setVertices(b);
+                    stations.add(info);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.i("JSON", "Couldn't do it!");
+            }
 
+            Log.i("Station", stations.toString());
+            for(i = 0; i < stations.size(); i++) {
+                stationNames.add(stations.get(i).getName());
+            }
+            adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, stationNames);
+            source.setAdapter(adapter);
+            destination.setAdapter(adapter);
+
+            loadingSpinner.setVisibility(View.GONE);
+            showRoute.setVisibility(View.VISIBLE);
         }
 
+    }
+
+    public boolean validate() {
+        String a = String.valueOf(source.getText());
+        String b = String.valueOf(destination.getText());
+        boolean a_status = true, b_status = true;
+        if(a.equals("")) {
+            source.setError("Enter a station's name.");
+        }
+        if(b.equals("")) {
+            destination.setError("Enter a station's name.");
+        }
+        if(!stationNames.contains(a) && !a.equals("")) {
+            source.setError("Station does not exist.");
+            a_status = false;
+        }
+        if(!stationNames.contains(b) && !b.equals("")) {
+            destination.setError("Station does not exist.");
+            b_status = false;
+        }
+        return a_status && b_status;
+    }
+
+    public void loadStationData() {
+        new GetStationData().execute("http://delhi-metro-api.herokuapp.com/distance");
     }
 }
--- DelhiMetroPathFinder/app/src/main/java/com/darksidehacks/delhimetropathfinder/activity/StationInfo.java
+++ DelhiMetroPathFinder/app/src/main/java/com/darksidehacks/delhimetropathfinder/activity/StationInfo.java
@@ -1,5 +1,7 @@
 package com.darksidehacks.delhimetropathfinder.activity;
 
+import java.util.ArrayList;
+
 /**
  * Created by kartik on 11/11/17.
  */
@@ -8,4 +10,43 @@ public class StationInfo {
 
     private String name;
     private String line;
+    private int index;
+    private ArrayList<Integer> vertices;
+    String LINE_SUFFIX = " Line";
+
+    public StationInfo() {
+        vertices = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public ArrayList<Integer> getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(ArrayList<Integer> vertices) {
+        this.vertices = vertices;
+    }
 }
